d8c00ca17b3ab8c1d831adbbe1969eaf15266e9b
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2024-04-26 01:30:35 +0300

    website-structure

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImage }) => {
+  return (
+    <div
+      className={`border-2 rounded-xl ${
+        bigShoeImage === imgURL ? "border-coral-red" : "border-transparent"
+      } cursor-pointer`}
+    ></div>
+  );
+};
+
+export default ShoeCard;
